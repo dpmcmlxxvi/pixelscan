96f79f2a7acf20a39255e0fa53ca6169189206bd
dpmcmlxxvi <dpmcmlxxvi@gmail.com>
2015-12-04 13:46:24 -0500

    Fix code smells.

--- examples/examples.py
+++ examples/examples.py
@@ -1,43 +1,46 @@
 #!/usr/bin/python
 
-from pixelscan.pixelscan import *
+from pixelscan.pixelscan import (chebyshev, circlescan, gridscan, hilbertscan,
+    manhattan,  ringscan, skip, snap, snakescan, walkscan)
+
+import random
 
 def printpoints(filename, points):
 
-    with open(filename, "w") as file:
+    with open(filename, "w") as f:
         for x, y in points:
-            file.write("{},{}\n".format(x, y))
+            f.write("{},{}\n".format(x, y))
 
 def main():
 
-        x0, y0, r1, r2 = 0, 0, 0, 2
-        points = snap(circlescan(x0, y0, r1, r2))
-        printpoints("circlescan.csv", points)
+    x0, y0, r1, r2 = 0, 0, 0, 2
+    points = snap(circlescan(x0, y0, r1, r2))
+    printpoints("circlescan.csv", points)
 
-        x0, y0, x1, y1 = 0, 0, 2, 2
-        points = gridscan(x0, y0, x1, y1)
-        printpoints("gridscan.csv", points)
+    x0, y0, x1, y1 = 0, 0, 2, 2
+    points = gridscan(x0, y0, x1, y1)
+    printpoints("gridscan.csv", points)
 
-        size, distance = 4, 16
-        points = hilbertscan(size, distance)
-        printpoints("hilbertscan.csv", points)
+    size, distance = 4, 16
+    points = hilbertscan(size, distance)
+    printpoints("hilbertscan.csv", points)
 
-        x0, y0, r1, r2 = 0, 0, 0, 2
-        points = ringscan(x0, y0, r1, r2, metric=chebyshev)
-        printpoints("chebyshev.csv", points)
+    x0, y0, r1, r2 = 0, 0, 0, 2
+    points = ringscan(x0, y0, r1, r2, metric=chebyshev)
+    printpoints("chebyshev.csv", points)
 
-        x0, y0, r1, r2 = 0, 0, 0, 2
-        points = ringscan(x0, y0, r1, r2, metric=manhattan)
-        printpoints("manhattan.csv", points)
+    x0, y0, r1, r2 = 0, 0, 0, 2
+    points = ringscan(x0, y0, r1, r2, metric=manhattan)
+    printpoints("manhattan.csv", points)
 
-        x0, y0, x1, y1 = 0, 0, 2, 2
-        points = snakescan(x0, y0, x1, y1)
-        printpoints("snakescan.csv", points)
+    x0, y0, x1, y1 = 0, 0, 2, 2
+    points = snakescan(x0, y0, x1, y1)
+    printpoints("snakescan.csv", points)
 
-        random.seed(0)
-        x0, y0 = 0, 0
-        points = skip(walkscan(x0, y0), stop=8)
-        printpoints("walkscan.csv", points)
+    random.seed(0)
+    x0, y0 = 0, 0
+    points = skip(walkscan(x0, y0), stop=8)
+    printpoints("walkscan.csv", points)
 
 if __name__ == "__main__":
     main()
